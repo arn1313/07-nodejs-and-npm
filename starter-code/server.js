42398a491d3da1a46f5db57f503b772930f4d1bd
aaron mcpeek <aaronmcpeek@aarons-MacBook-Pro.local>
2017-07-19 09:56:30 -0700

    more todos

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,6 +1,6 @@
 'use strict';
 
-// TODO: Initialize your project using NPM to create and populate a package.json file
+// DONE: Initialize your project using NPM to create and populate a package.json file
 let express = require('express');
 // TODO: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
@@ -9,8 +9,8 @@ let express = require('express');
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
-
+// DONE: Include all of the static resources as an argument to app.use()
+app.use(express.static('./public'));
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
 
